9b42db66c3364d86ae864084c35d27cead30eb46
lynxrelax <lynxrelax@yahoo.com>
2022-01-06 22:50:29 -0500

    data normalization

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,21 +1,23 @@
-import React from "react";
-import './collections-overview.styles.scss';
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import  CollectionPreview from "../preview-collection/preview-collection.component";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-const CollectionOverview = ({collections}) =>(
-  <div className="collections-overview">
-    { collections.map( ({id,...otherSectionProps}) =>(
-            <CollectionPreview key={id} {...otherSectionProps} />
-        ))
-        }
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+
+import './collections-overview.styles.scss';
+
+const CollectionsOverview = ({ collections }) => (
+  <div className='collections-overview'>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections:selectCollections
+  collections: selectCollectionsForPreview
 });
 
-export default connect(mapStateToProps)(CollectionOverview);
+export default connect(mapStateToProps)(CollectionsOverview);
